89bb5b17311e1f4a445b60d0b1dd0a91e70f709a
viniciussilvaribeiro <130010792+viniciussilvaribeiro@users.noreply.github.com>
2023-11-27 11:26:40 -0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,13 +1,26 @@
-from django.contrib import admin
 from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+from .views import OwnerCreateView, LocalCreateViewRemove
 
-from devices.urls import urlpatterns as devices_urls
+# owner_router = DefaultRouter()
+# owner_router.register(r'owner', OwnerCreate, basename = 'owner')
 
+# local_router = DefaultRouter()
+# local_router.register(r'local', LocalViewSet, basename = 'local')
+
+# enviroment_router = DefaultRouter()
+# enviroment_router.register(r'enviroment', EnviromentViewSet, basename = 'enviroment')
+
+# device_router = DefaultRouter()
+# device_router.register(r'device', DeviceViewSet, basename = 'device')
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-    
-    path('smarthome/', include(devices_urls)),
-
+    path('owner/', OwnerCreateView.as_view(), name = 'owner_create'),
+    path('owner/<int:owner_id>', OwnerCreateView.as_view(), name = 'owner_view'),
+    path('owner/<int:owner_id>/local/', LocalCreateViewRemove.as_view(), name = 'local_create'),
+    path('owner/<int:owner_id>/local/<int:local_id>', LocalCreateViewRemove.as_view(), name = 'local_view_delete'),
+    # path('owner/<int:owner_id>/', include(owner_router.urls)),
+    # path('owner/<int:owner_id>/local/<int:local_id>/', include(local_router.urls)),
+    # path('owner/<int:owner_id>/local/<int:local_id>/enviroment/<int:enviroment_id>/', include(enviroment_router.urls)),
+    # path('owner/<int:owner_id>/local/<int:local_id>/enviroment/<int:enviroment_id>/device/<int:device_id>', include(device_router.urls)),
 ]
